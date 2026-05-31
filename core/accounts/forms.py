c4d57202d6f3b7b5e92d8c9708b59475f88c5e8c
mmd-punisher <mohammadmoosapoor4@gmail.com>
2026-05-31 20:42:41 +0330

    feat: register form, page and styles

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,24 +1,43 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-input'})
+    )
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+
+    avatar = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'class': 'form-input'})
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'avatar']
+        fields = ['username', 'email', 'avatar', 'password1', 'password2']
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
+
         if commit:
             user.save()
-            if self.cleaned_data.get('avatar'):
-                user.avatar = self.cleaned_data['avatar']
-                user.save()
+
         return user
 
 

6ccc46795c1de8a6aaa5124584d275a5c58260e5
mmd-punisher <mohammadmoosapoor4@gmail.com>
2026-05-31 21:18:00 +0330

    feat: task delete page and add

--- core/tasks/forms.py
+++ core/tasks/forms.py
@@ -7,3 +7,20 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
         fields = ['title', 'description', 'is_completed']
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Enter task title'
+            }),
+
+            'description': forms.Textarea(attrs={
+                'class': 'form-input',
+                'placeholder': 'Enter description',
+                'rows': 4
+            }),
+
+            'is_completed': forms.CheckboxInput(attrs={
+                'class': 'form-checkbox'
+            }),
+        }

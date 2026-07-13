0badc8cfc557593291245137fce5698bc21f5dd5
mmdend <mmdend.dev@gmail.com>
2026-07-13 22:02:56 +0330

    feat: basic rest ali auth

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls', namespace='accounts')),
-    path('todo/', include('tasks.urls', namespace='tasks')),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("", include("accounts.urls", namespace="accounts")),
+    path("todo/", include("tasks.urls", namespace="tasks")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

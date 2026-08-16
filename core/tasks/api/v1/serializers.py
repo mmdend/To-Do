a12a365b07337682fa94c4e8919cedda065bf0dd
mmdend <mmdend.dev@gmail.com>
2026-08-16 19:26:49 +0330

    feat(tasks:serializer): absolute url added

--- core/tasks/api/v1/serializers.py
+++ core/tasks/api/v1/serializers.py
@@ -5,11 +5,24 @@ from ...models import Tasks
 
 class TaskSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
+    absolute_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Tasks
-        fields = ("id", "title", "description", "snippet", "is_completed", "created_at")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "snippet",
+            "absolute_url",
+            "is_completed",
+            "created_at",
+        )
         read_only_fields = ("id", "created_at")
+
+    def get_absolute_url(self, obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(obj)
 
     def create(self, validated_data):
         validated_data["user"] = self.context["request"].user
